25fe7d6e7af31041714e64db3432826ae63ad9cd
bruceeewong <292803281@qq.com>
2019-07-30 00:12:25 +0800

    finish 5-2

---
+++ src/components/panel.vue
@@ -0,0 +1,35 @@
+<template>
+  <section :class="[panelClass, cname]">
+    <h4>-{{ title }}-</h4>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'panel',
+  props: {
+    cname: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      panelClass: 'panel'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/css/element.scss";
+
+.panel {
+  @include panel;
+}
+</style>
---
+++ src/components/slider.vue
@@ -0,0 +1,64 @@
+<template>
+  <section :class="cname">
+    <swiper 
+      :options="options"
+      :not-next-tick="options.notNextTick"
+    >
+      <swiper-slide
+        v-for="item in items"
+        :key="item.href"
+      >
+        <router-link :to="{ name: item.href }">
+          <img :src="item.src" alt="">
+        </router-link>
+        <div
+          v-if="options.pagination"
+          class="swiper-pagination"
+        ></div>
+      </swiper-slide>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  name: 'slider',
+  props: {
+    options: {
+      type: Object,
+      default () {
+        return {
+          autoplay: true,
+          loop: true,
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          notNextTick: false
+        }
+      }
+    },
+    items: {
+      type: Array,
+      default () {
+        return [
+          {
+            href: '',
+            src: ''
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~swiper/dist/css/swiper.css';
+
+</style>
